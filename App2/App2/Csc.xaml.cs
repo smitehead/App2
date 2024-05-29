a717efea72d37217124e5c5cfeceeef497773c3c
smitehead <dkdldjdy12@naver.com>
2024-05-29 18:26:55 +0900

    네비추가

--- App2/App2/Csc.xaml.cs
+++ App2/App2/Csc.xaml.cs
@@ -1,20 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
 
 namespace App2
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class Csc : ContentPage
-	{
-		public Csc ()
-		{
-			InitializeComponent ();
-		}
-	}
+    public partial class Csc : ContentPage
+    {
+        public Csc()
+        {
+            InitializeComponent();
+
+            // 로컬 HTML 파일 로드
+            var htmlSource = new HtmlWebViewSource
+            {
+                Html = @"<!DOCTYPE html>
+                        <html>
+                        <head>
+                            <title>Google Map</title>
+                            <meta name='viewport' content='initial-scale=1.0, user-scalable=no'>
+                            <meta charset='utf-8'>
+                            <script src='https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY'></script>
+                            <script>
+                                function initMap() {
+                                    var location = { lat: 37.7749, lng: -122.4194 };
+                                    var map = new google.maps.Map(document.getElementById('map'), {
+                                        zoom: 10,
+                                        center: location
+                                    });
+                                    var marker = new google.maps.Marker({
+                                        position: location,
+                                        map: map
+                                    });
+                                }
+                            </script>
+                        </head>
+                        <body onload='initMap()'>
+                            <div id='map' style='height: 100%; width: 100%;'></div>
+                        </body>
+                        </html>"
+            };
+
+            GoogleMapWebView.Source = htmlSource;
+        }
+    }
 }
